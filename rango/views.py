2123655cbe80b723fe7fdc5f65673d56eb5d7dbf
Jamie <2560013v@student.gla.ac.uk>
2022-01-31 12:11:38 +0000

    Models and Admin interface

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,8 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': 'About'}
+    return render(request,'rango/about.html', context = context_dict)
     
